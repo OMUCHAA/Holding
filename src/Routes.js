40cf26d8015b53f249d6f336ebcaa200938508be
OMUCHAA <shemomu42@gmail.com>
2026-06-23 16:26:23 +0300

    adding the Hash history back for app testing.

--- src/Routes.js
+++ src/Routes.js
@@ -1,4 +1,4 @@
-import { createWebHistory, createRouter } from "vue-router";
+import { createWebHashHistory, createRouter } from "vue-router";
 
 const Home = () => import("./components/Home.vue");
 const Contacts = () => import("./components/Contacts.vue");
@@ -106,7 +106,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes,
 
   scrollBehavior(to, from, savedPosition) {
