0651273c3fc3fdc61e94c886474a9864e578f560
OMUCHAA <shemomu42@gmail.com>
2026-06-23 16:14:09 +0300

    changing the background of the Footer.

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,15 +1,20 @@
 <template>
-  <footer class="bg-gradient-to-bl from-black via-yellow-800 to-black text-gray-300 py-8 text-center">
-        <div class="max-w-7xl mx-auto px-6 grid grid-cols-1 md:grid-cols-3 gap-8 text-center md:text-left">
-      
+  <footer
+    class="bg-gradient-to-r from-black via-amber-900 to-black text-gray-300 py-8 text-center"
+  >
+    <div
+      class="max-w-7xl mx-auto px-6 grid grid-cols-1 md:grid-cols-3 gap-8 text-center md:text-left"
+    >
       <!-- Company Info -->
       <div>
         <h2 class="text-2xl font-bold mb-3 text-amber-300">MASHBAH HOLDINGS</h2>
         <div class="flex md:justify-start justify-center mb-4">
-            <img src="../assets/image.png" alt="Company-Logo" class="h-12">
+          <img src="../assets/image.png" alt="Company-Logo" class="h-12" />
         </div>
         <p class="text-amber-400">
-          MashBah Holdings is a diversified company that manages and invests in high-potential businesses across multiple sectors to drive growth and long-term value
+          MashBah Holdings is a diversified company that manages and invests in
+          high-potential businesses across multiple sectors to drive growth and long-term
+          value
         </p>
       </div>
 
@@ -17,28 +22,45 @@
       <div>
         <h3 class="text-lg font-semibold mb-3 text-amber-300">Quick Links</h3>
         <ul class="space-y-2 text-blue-300">
-          <li><router-link to="/" class="hover:text-white transition">Home</router-link></li>
-          <li><router-link to="/contacts" class="hover:text-white transition">Contact</router-link></li>
-          <li><router-link to="/about" class="hover:text-white transition">About</router-link></li>
-          <li><router-link to="/subsidiaries" class="hover:text-white transition">Our Companies</router-link></li>
+          <li>
+            <router-link to="/" class="hover:text-white transition">Home</router-link>
+          </li>
+          <li>
+            <router-link to="/contacts" class="hover:text-white transition"
+              >Contact</router-link
+            >
+          </li>
+          <li>
+            <router-link to="/about" class="hover:text-white transition"
+              >About</router-link
+            >
+          </li>
+          <li>
+            <router-link to="/subsidiaries" class="hover:text-white transition"
+              >Our Companies</router-link
+            >
+          </li>
         </ul>
       </div>
-            
+
       <!-- Contact Info -->
       <div class="text-amber-300">
         <h3 class="text-lg font-bold mb-3">Get in Touch</h3>
-        <p class="text-amber-500"> MSA Rd, Kenya</p>
+        <p class="text-amber-500">MSA Rd, Kenya</p>
         <p class="text-amber-500">+254 780818238</p>
-        <p class="text-amber-500">sales@mashbahholdings.co.ke <br> accounts@mashbahholdings.co.ke</p>
+        <p class="text-amber-500">
+          sales@mashbahholdings.co.ke <br />
+          accounts@mashbahholdings.co.ke
+        </p>
       </div>
     </div>
-    <div class="border-t border-amber-200 flex flex-col justify-center text-amber-100 items-center mt-3 pt-3">
+    <div
+      class="border-t border-amber-200 flex flex-col justify-center text-amber-100 items-center mt-3 pt-3"
+    >
       <p>© {{ new Date().getFullYear() }} MashBah Holdings. All rights reserved.</p>
       <router-link class="hover:text-blue-100" to="/">Back to top</router-link>
     </div>
   </footer>
 </template>
 
-<script setup>
-
-</script>
+<script setup></script>
