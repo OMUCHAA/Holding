6565a7bd49645c843d4ce9465c1b8cb8e66a4010
OMUCHAA <shemomu42@gmail.com>
2026-06-23 16:05:55 +0300

    Improving the navigation menu.

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,77 +1,77 @@
 <template>
-  <nav class="border shadow p-6 fixed w-full z-50 top-0 bg-amber-50">
-    <div class="flex justify-around items-center">
-      <div
-        id="logo"
-        class="ml-4 font-semibold text-2xl md:text-3xl text-gold-900"
-      >
-        <span class="italic font-bold"
-          ><img
-            src="../assets/image.png"
-            class="lg:h-20 md:h-16 h-14"
-            alt="logo"
-        /></span>
-      </div>
+  <nav class="fixed top-0 z-50 w-full border-b border-amber-200 bg-white shadow-sm">
+    <div
+      class="mx-auto flex max-w-7xl items-center justify-between px-4 py-4 sm:px-6 lg:px-8"
+    >
+      <!-- LOGO -->
+      <router-link to="/" class="flex items-center gap-3">
+        <img
+          src="../assets/image.png"
+          alt="logo"
+          class="h-12 md:h-14 lg:h-16 w-auto object-contain"
+        />
+      </router-link>
 
-      <!--Horizontal Menu Hidden on small screens-->
+      <!-- DESKTOP MENU -->
       <ul
-        class="mr-10 text-xl font-semibold hidden lg:flex text-amber-900 space-x-8 items-center"
+        class="hidden lg:flex items-center gap-10 text-[16px] font-medium text-amber-900"
       >
-        <li class="mr-6 relative group font-medium">
-          <router-link to="/">Home</router-link>
-        </li>
-        <li class="relative group font-medium">
-          <router-link to="/subsidiaries">Companies</router-link>
-        </li>
-        <li class="mr-6 relative group font-medium">
-          <router-link to="/about">About Us</router-link>
-        </li>
-        <li class="mr-6 relative group font-medium">
-          <router-link to="/contacts">Contacts</router-link>
+        <li v-for="item in navItems" :key="item.name">
+          <router-link
+            :to="item.to"
+            class="relative transition hover:text-black"
+            active-class="text-black font-semibold"
+          >
+            <span
+              class="after:absolute after:left-0 after:-bottom-1 after:h-[2px] after:w-0 after:bg-amber-700 after:transition-all hover:after:w-full"
+            >
+              {{ item.name }}
+            </span>
+          </router-link>
         </li>
       </ul>
 
-      <!--What is going to be visible on smaller screens-->
-      <!-- Mobile Menu Toggle Button (bars only) -->
-      <div @click="toggleMenu" class="lg:hidden">
-        <i v-if="!isOpen" class="fa-solid fa-bars text-4xl text-amber-900"></i>
-      </div>
-
-      <!-- Slide-in Mobile Menu -->
-      <div
-        class="fixed top-0 right-0 h-full w-1/2 max-w-xs bg-gradient-to-bl from-black via-yellow-800 to-black text-amber-300 p-4 shadow-lg transform transition-transform duration-300 lg:hidden"
-        :class="isOpen ? 'translate-x-0' : 'translate-x-full'"
-      >
-        <!-- X Close Button INSIDE Sidebar -->
-        <button @click="closeMenu" class="absolute top-4 left-4">
-          <i class="fa-solid fa-xmark text-4xl text-amber-500"></i>
-        </button>
-
-        <!-- Menu Items -->
-        <ul class="flex flex-col gap-4 mt-16 font-semibold text-lg">
-          <li @click="closeMenu">
-            <router-link to="/" class="block hover:text-blue-600"
-              >Home</router-link
-            >
-          </li>
-          <li @click="closeMenu">
-            <router-link to="/subsidiaries" class="block hover:text-blue-600"
-              >Subsidiaries</router-link
-            >
-          </li>
-          <li @click="closeMenu">
-            <router-link to="/about" class="block hover:text-blue-600"
-              >About Us</router-link
-            >
-          </li>
-          <li @click="closeMenu">
-            <router-link to="/contacts" class="block hover:text-blue-600"
-              >Contacts</router-link
-            >
-          </li>
-        </ul>
-      </div>
+      <!-- MOBILE BUTTON -->
+      <button @click="toggleMenu" class="lg:hidden text-3xl text-amber-900">
+        <i class="fa-solid fa-bars"></i>
+      </button>
     </div>
+
+    <!-- OVERLAY -->
+    <div
+      v-if="isOpen"
+      @click="closeMenu"
+      class="fixed inset-0 bg-black/40 lg:hidden"
+    ></div>
+
+    <!-- MOBILE DRAWER -->
+    <aside
+      class="fixed top-0 right-0 h-full w-[78%] max-w-xs bg-gradient-to-b from-black via-amber-900 to-black text-amber-200 shadow-2xl transform transition-transform duration-300 lg:hidden"
+      :class="isOpen ? 'translate-x-0' : 'translate-x-full'"
+    >
+      <!-- HEADER -->
+      <div class="flex items-center justify-between p-5 border-b border-amber-700/30">
+        <span class="text-lg font-semibold text-amber-300">Menu</span>
+
+        <button @click="closeMenu">
+          <i class="fa-solid fa-xmark text-2xl text-amber-400"></i>
+        </button>
+      </div>
+
+      <!-- LINKS -->
+      <ul class="flex flex-col gap-2 p-5 text-lg">
+        <li v-for="item in navItems" :key="item.name">
+          <router-link
+            :to="item.to"
+            @click="closeMenu"
+            class="block rounded-lg px-3 py-3 transition hover:bg-amber-800/40"
+            active-class="bg-amber-800/60 text-white"
+          >
+            {{ item.name }}
+          </router-link>
+        </li>
+      </ul>
+    </aside>
   </nav>
 </template>
 
@@ -79,6 +79,7 @@
 import { ref } from "vue";
 
 const isOpen = ref(false);
+
 function toggleMenu() {
   isOpen.value = !isOpen.value;
 }
@@ -86,4 +87,11 @@ function toggleMenu() {
 function closeMenu() {
   isOpen.value = false;
 }
+
+const navItems = [
+  { name: "Home", to: "/" },
+  { name: "Companies", to: "/subsidiaries" },
+  { name: "About Us", to: "/about" },
+  { name: "Contacts", to: "/contacts" },
+];
 </script>
